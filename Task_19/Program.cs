4ef273be0ac8465bfc92eb434ddf2e76406dc265
G0ryny4 <gorynych90@gmail.com>
2022-07-22 19:18:44 +0300

    added task 19, 21 and 23

---
+++ Task_19/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int temp = num;
+int revers = 0;
+int digit = 0;
+
+string Palindrome(int num)
+{
+    while (num > 0)
+    {
+        digit = num % 10;
+        revers = revers * 10 + digit;
+        num = num / 10;
+
+    }
+    if (temp == revers) return "ДА";
+    return "НЕТ";
+}
+
+string result = Palindrome(num);
+
+Console.WriteLine($"{num} -> {result}");
---
+++ Task_23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+ShowCube(num);
+
+void ShowCube(int n)
+{
+    int i = 1;
+    while (i <=n)
+    {
+        Console.WriteLine($"| {i} | {i * i * i, 3} |");
+        i++;
+    }
+    return;
+}   
